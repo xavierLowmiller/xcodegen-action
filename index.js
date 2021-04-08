2b79982072b4c5a18c4ff9323b1dc2b296fdbb05
Xavier Lowmiller <xaver.lohmueller@me.com>
2021-04-08 08:33:30 +0200

    Load latest release if version is 'latest'

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-const core = require('@actions/core');
+const core = require('@actions/core')
 const exec = require('@actions/exec')
-const os = require('os');
+const https = require('https')
+const os = require('os')
 
 try {
   main()
@@ -12,18 +13,53 @@ async function installXcodegen() {
   const xcodegenDir = os.homedir() + '/action-xcodegen/'
   const zipFile = xcodegenDir + 'xcodegen.zip'
   const version = core.getInput('version')
+  let url
+  if (version == 'latest') {
+    url = await loadLatestReleaseUrl()
+  } else {
+    url = `https://github.com/yonaskolb/XcodeGen/releases/download/${version}/xcodegen.zip`
+  }
   await exec.exec('curl', [
     '--silent',
     '--create-dirs',
     '--location',
     '--output',
     zipFile,
-    `https://github.com/yonaskolb/XcodeGen/releases/download/${version}/xcodegen.zip`
+    url
   ])
   await exec.exec('unzip', ['-q', '-o', zipFile], { cwd: xcodegenDir })
   await exec.exec('rm', [zipFile])
   await exec.exec('xcodegen/install.sh', null, { cwd: xcodegenDir })
   await exec.exec('rm -rf', [xcodegenDir])
+}
+
+async function loadLatestReleaseUrl() {
+  const options = {
+    hostname: 'api.github.com',
+    path: '/repos/yonaskolb/XcodeGen/releases/latest',
+    headers: { 'User-Agent': 'xcodegen-github-action' }
+  }
+
+  const promise = new Promise((resolve, reject) => {
+    https.get(options, res => {
+      res.setEncoding("utf8");
+      let body = ""
+      res.on("data", data => {
+        body += data
+      })
+      res.on("end", () => {
+        const json = JSON.parse(body)
+        resolve(json)
+      })
+      res.on("error", error => {
+        console.log(error)
+        reject(error)
+      })
+    })
+  })
+  const json = await promise
+
+  return json["assets"][0]["browser_download_url"]
 }
 
 async function runXcodegen() {
